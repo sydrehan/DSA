b02156c5804094a0c18d6c51a842b7489d4390c6
Rehan Raza <rehanrazasayed@gmail.com>
2024-10-27 17:05:55 +0530

    init

--- digit_count.cpp
+++ digit_count.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     int n;
+    cout<<"Enter the Number: ";
     cin>>n;
     int count=0;
     while(n>0){
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
-#include<algorithm>
 #include<string>
 using namespace std;
+int cnt=1;
+void print(int n){
+    if(cnt>n)return;
+    cout<<cnt<<" ";
+    cnt++;
+    print(n);
+}
 int main()
 {
-    int n1,n2;
-    cout<<"enter 2 numbers: ";
-    cin>>n1>>n2;
-    int gcd=1;
-    for(int i=1;i<min(n1,n2);i++){
-        if(n1%i==0 && n2%i==0){
-            gcd=i;
-            cout<<i<<endl;
-        }
-    }
- return 0;
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    print(n);
+    cout<<endl;
+    return 0;
 }
